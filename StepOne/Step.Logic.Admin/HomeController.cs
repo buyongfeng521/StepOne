592092299b896846223ab514fcaef5e6fae9e9d2
buyongfeng521 <309929800@qq.com>
2015-04-04 20:28:47 +0800

    前台架子
    前台架子

---
+++ StepOne/Step.Logic.Admin/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Step.Logic.Admin
+{
+    public class HomeController:Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
