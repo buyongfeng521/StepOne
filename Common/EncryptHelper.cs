70fb1b54c0dd6773ca89b07f8473c0daab3aabdb
step <buzidongdong@163.com>
2015-06-16 09:33:44 +0800

    修正

---
+++ Common/EncryptHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class EncryptHelper
+    {
+        #region MD5加密
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static string GetMD5(string data)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] bytValue, bytHash;
+            bytValue = System.Text.Encoding.UTF8.GetBytes(data);
+            bytHash = md5.ComputeHash(bytValue);
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        } 
+        #endregion
+
+
+
+    }
+}
