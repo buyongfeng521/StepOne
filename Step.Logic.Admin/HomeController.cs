fd27dd7b8c33459a78dd18e2f02701a9d896c5ce
step <buzidongdong@163.com>
2015-06-16 18:52:07 +0800

    2

--- Step.Logic.Admin/HomeController.cs
+++ Step.Logic.Admin/HomeController.cs
@@ -14,22 +14,34 @@ namespace Step.Logic.Admin
         [HttpGet]
         public ActionResult Login()
         {
-            Sys_User user = OperateContext.BLLSession.ISys_UserBLL.GetModelBy(u => u.UName == "buyf");
-            if (user != null)
-            {
-                user.UName = "buyf";
-                user.UPassword = Common.EncryptHelper.GetMD5("888888");
-                user.URealName = "buyongfeng";
-                user.UEmail = "xxx@163.com";
-                user.UTel = "136XXXXXXXX";
-                user.Note = "备注";
-                user.IsDel = 0;
-                user.CreateDate = DateTime.Now;
-                user.ModifyDate = DateTime.Now;
-                OperateContext.BLLSession.ISys_UserBLL.Modify(user);
-            }
-            ViewBag.IsSuc = "成功";
+            //Sys_User user = OperateContext.BLLSession.ISys_UserBLL.GetModelBy(u => u.UName == "buyf");
+            //if (user != null)
+            //{
+            //    user.UName = "buyf";
+            //    user.UPassword = Common.EncryptHelper.GetMD5("888888");
+            //    user.URealName = "buyongfeng";
+            //    user.UEmail = "xxx@163.com";
+            //    user.UTel = "136XXXXXXXX";
+            //    user.Note = "备注";
+            //    user.IsDel = 0;
+            //    user.CreateDate = DateTime.Now;
+            //    user.ModifyDate = DateTime.Now;
+            //    OperateContext.BLLSession.ISys_UserBLL.Modify(user);
+            //}
+            //ViewBag.IsSuc = "成功";
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(Sys_User user)
+        {
+            string strPassword = Common.EncryptHelper.GetMD5(user.UPassword);
+            if (OperateContext.BLLSession.ISys_UserBLL.GetModelBy(u => u.UName == user.UName && u.UPassword == strPassword) != null)
+            {
+                //Redirect("~/home/index/");
+            }
+            return View();    
+        }
+
     }
 }
