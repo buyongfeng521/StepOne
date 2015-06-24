d743154ad9870a69e22f113468d90ca80f899e11
step <buzidongdong@163.com>
2015-06-24 21:30:44 +0800

    333

--- Step.Logic.Admin/HomeController.cs
+++ Step.Logic.Admin/HomeController.cs
@@ -29,6 +29,15 @@ namespace Step.Logic.Admin
             //    OperateContext.BLLSession.ISys_UserBLL.Modify(user);
             //}
             //ViewBag.IsSuc = "成功";
+
+            //Sys_User sys_User = OperateContext.BLLSession.ISys_UserBLL.GetListBy(u => u.UID == 1).FirstOrDefault();
+            //if (sys_User != null)
+            //{
+            //    sys_User.UName = "buyf";
+            //    sys_User.UPassword = Common.EncryptHelper.GetMD5("888888");
+
+            //    OperateContext.BLLSession.ISys_UserBLL.Modify(sys_User);
+            //}
             
             return View();
         }
@@ -42,9 +51,16 @@ namespace Step.Logic.Admin
                 if (OperateContext.BLLSession.ISys_UserBLL.GetModelBy(u => u.UName == user.UName && u.UPassword == strPassword) != null)
                 {
                     //Response.Redirect("/Home/Index");
-                    Response.Redirect("Index");
+                    //Response.Redirect("Index");
+                    return Content("ok");
+                }
+                else
+                {
+                    //return Content("<script>alert('用户名或密码错误');window.location.href='/Admin/Home/Login';</script>", "text/html");
+                    return Content("密码或用户名错误");
                 }
             }
+            
             return View();    
         }
 
@@ -62,6 +78,11 @@ namespace Step.Logic.Admin
         public ActionResult FormElement()
         {
             return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return Redirect("/Admin/Home/Login");
         }
 
 
